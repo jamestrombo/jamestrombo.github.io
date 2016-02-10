bfd5d2ccab484271e80aa6ba6605aaab92c2cc4c
DarkReaperRising <jamestrombo@gmail.com>
2016-02-10 11:22:24 -0500

    lmao

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function email() {
+window.open('mailto:jamestrombo@gmail.com');
+}
+$(document).ready(function() {
+    alert('This is a site in progress, dont be harsh bruh')
+});
--- scripts2.js
+++
@@ -1,6 +0,0 @@
-$(document).on('scroll', function(e) {
-var value = $(this).scrollTop();
-    if (value < 200) {
-    	$('lmao').toggleClass('open');
-    });
-});
