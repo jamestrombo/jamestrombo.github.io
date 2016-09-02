22d64dfb6aae6714bc5b90d7bca7a5092101a83c
James A. Trombo <jamestrombo@gmail.com>
2016-09-02 13:33:59 -0400

    Magic

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 function email() {
 	window.open('mailto:jamestrombo@gmail.com');
 }
+window.addEventListener('scroll', function(e) {
+	console.log(window.scrollY);
+	if (window.scrollY >= 100){
+	    document.getElementsByTagName('header')[0].className = 'trigger';
+	}
+});
