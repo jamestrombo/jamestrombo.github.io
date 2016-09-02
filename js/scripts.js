316d7207f624dd7a4dd57b55b971d93cb9b52911
James A. Trombo <jamestrombo@gmail.com>
2016-09-02 18:36:56 -0400

    Animation works hooray

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 function email() {
 	window.open('mailto:jamestrombo@gmail.com');
 }
+headerup = false;
 window.addEventListener('scroll', function(e) {
 	console.log(window.scrollY);
-	if (window.scrollY >= 100){
+	if (headerup == false && window.scrollY >= 50){
+	    headerup = true;
 	    document.getElementsByTagName('header')[0].className = 'trigger';
+	} 
+	else if (headerup == true && window.scrollY <= 50){
+	    headerup = false;
+	    document.getElementsByTagName('header')[0].className = 'untrigger';
 	}
 });
