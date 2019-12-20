0c7ad005c19e4a4fe544d90ba7f72757fa746ed4
James Trombo <jamestrombo@gmail.com>
2019-12-20 10:38:19 -0500

    A lot of pretty big site aesthetic changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,38 @@
+var initial = false;
+var colorarray = [
+	"#D81B60", "#8E24AA", "#E53935", "#5E35B1", "#3949AB",
+	"#1E88E5", "#039BE5", "#00ACC1", "#00897B", "#43A047"
+]
 function email() {
 	window.open('mailto:jamestrombo@gmail.com');
 }
+function backgroundColor() {
+	console.log("her");
+	if (!initial){
+		$("#header").css({ backgroundColor: colorarray[Math.floor(Math.random()*colorarray.length)],});
+	}
+	if (initial) {
+		$("#header").velocity({
+				backgroundColor: colorarray[Math.floor(Math.random()*colorarray.length)],
+				}, {
+				duration: 1000,
+			});	
+	intial = true;
+}
+}	
+
 headerup = false;
 window.addEventListener('scroll', function(e) {
 	console.log(window.scrollY);
 	if (headerup == false && window.scrollY >= 50){
 	    headerup = true;
-	    document.getElementsByTagName('header')[0].className = 'trigger';
+	    document.getElementsByTagName('header')[0].className = 'trigger';    
 	} 
 	else if (headerup == true && window.scrollY <= 50){
 	    headerup = false;
 	    document.getElementsByTagName('header')[0].className = 'untrigger';
+	    backgroundColor();
+	    
 	}
 });
+setInterval(backgroundColor(), 3000);
